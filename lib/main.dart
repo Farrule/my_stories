32d3ae9551542d904e6cf163fccec6c7a71303e6
farrule <tsukasap0921@gmail.com>
2022-01-12 12:01:56 +0900

    album add func

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:my_stories/components/provider/album.dart';
 // * theme file
 import 'package:my_stories/model/constructors/constructor.dart';
 // * route files
-import 'package:my_stories/screens/home.dart';
+import 'package:my_stories/screens/home/home_screen.dart';
 import 'package:my_stories/screens/welcome/welcome_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(ChangeNotifierProvider<AlbumData>(
+  create: (context) => AlbumData(),
+  child: MyApp(),
+));
 
 class MyApp extends StatelessWidget {
   @override
